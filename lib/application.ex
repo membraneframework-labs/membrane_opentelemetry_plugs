4ee27367a0b1cf176827f1edfbe127a49923a385
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2024-11-20 19:25:59 +0100

    Add warn if config contains non-existing plug

--- lib/application.ex
+++ lib/application.ex
@@ -8,6 +8,15 @@ defmodule Membrane.OpenTelemetry.Plugs.Application do
   @plugs_in_config Application.compile_env(:membrane_opentelemetry_plugs, :plugs, [])
   @enabled_plugs @defined_plugs |> Enum.filter(& &1 in @plugs_in_config)
 
+  Enum.each(@plugs_in_config, fn plug ->
+    if plug not in @defined_plugs do
+      IO.warn("""
+      Plug #{inspect(plug)} is present in :membrane_opentelemetry_plugs config, but \
+      it is not an available plug module. Available plug modules are: #{inspect(@defined_plugs)}
+      """)
+    end
+  end)
+
   @impl true
   def start(_type, _args) do
     @enabled_plugs
